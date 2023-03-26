8cfdecf5b9d5da427c0f06888335fdd9b3a41eb7
OfekExvoip <ofek@exvoip.com>
2023-03-26 08:16:09 +0000

    Terraform 2.0

--- ec2.tf
+++ ec2.tf
@@ -4,13 +4,25 @@ resource "aws_instance" "pbxec2" {
     key_name = "terraform-key"
     vpc_security_group_ids = ["${aws_security_group.pbxsg.id}"]
     subnet_id = "${aws_subnet.pbxsub1.id}"
+<<<<<<< HEAD
     tags = {
         Name = "Pbx-testform"
     }
+=======
+     depends_on = [
+    aws_vpc.pbxvpc
+  ]
+    tags = {
+        Name = "Pbx-testform"
+    }
+
+
+>>>>>>> 9affb1d (Terraform 2.0)
 }
 
 
 
+<<<<<<< HEAD
 
 
 
@@ -32,3 +44,12 @@ resource "aws_key_pair" "infr_exr_key" {
 }
 
 */
+=======
+resource null_resource "ansible_install" { 
+  
+  provisioner "local-exec" {
+command = "sleep 180; ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ubuntu --private-key /home/ubuntu/Terraform/terrakey -i '${aws_instance.pbxec2.public_ip},' ansible-script.yaml"
+}
+
+}
+>>>>>>> 9affb1d (Terraform 2.0)
